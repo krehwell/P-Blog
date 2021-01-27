e04bc08c55c994783e641587298b39a7e2356e80
krehwell <fahrimuham001@gmail.com>
2021-01-28 01:07:45 +0800

    WS-BE: setup express and mongoose

---
+++ frontend/rest-api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@coding-blog.rv1qo.mongodb.net/blog?retryWrites=true&w=majority`
+
+mongoose.connect(mongoString, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+mongoose.connection.on("error", (error) => {
+  if(process.env.NODE_ENV === "develoment"){
+    console.log(error);
+  }
+})
+
+mongoose.connection.on("open", () => {
+  console.log("Connected to MongoDB database");
+})
+
+app.use(helmet());
+
+app.use(require("./routes/index.js"));
+
+app.listen(PORT, () => {
+  console.log(`Express app listening on port ${PORT}`);
+})
