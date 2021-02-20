df590f743fa507d7222291d422411d0b549b7298
krehwell <fahrimuham001@gmail.com>
2021-02-21 03:03:56 +0800

    AD-FE: a basic login page with no api

---
+++ admin/website/pages/login.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import Head from "next/head";
+
+export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            credentialError: false,
+            emailInputValue: "",
+            emailRequiredError: false,
+            passwordInputValue: "",
+            passwordRequiredError: false,
+        };
+    }
+
+    updateEmailInputValue = (event) => {
+        this.setState({
+            emailInputValue: event.target.value,
+            emailRequiredError: false,
+        });
+    };
+
+    updatePasswordInputValue = (event) => {
+        this.setState({
+            passwordInputValue: event.target.value,
+            passwordRequiredError: false,
+        });
+    };
+
+    submitLoginRequest = () => {
+        this.setState({ loading: true });
+    };
+
+    render() {
+        return (
+            <div className="layout-wrapper">
+                <Head>
+                    <title>Login | Admin</title>
+                </Head>
+                <div className="login-wrapper">
+                    <div className="login-content-container">
+                        <div className="login-form-container">
+
+                            {this.state.credentialError ? (
+                                <div className="login-form-error-block">
+                                    <span>
+                                        Email address and/or password is
+                                        Incorrect
+                                    </span>
+                                </div>
+                            ) : null}
+
+                            <div className="login-form-top-header">
+                                <span>Admin Page</span>
+                            </div>
+
+                            <div className="login-form-field">
+                                <input
+                                    onChange={this.updateEmailInputValue}
+                                    value={this.state.emailInputValue}
+                                    type="email"
+                                    autoComplete="new-password"
+                                    placeholder="Email Adress"
+                                    className={ this.state.credentialError || this.state.emailRequiredError ? "error" : null }
+                                />
+                                {
+                                    this.state.emailRequiredError ?
+                                        <div className="login-form-msg">
+                                            <span>Email field is required.</span>
+                                        </div> : null
+                                }
+                            </div>
+
+                            <div className="login-form-field">
+                                <input
+                                    onChange={this.updatePasswordInputValue}
+                                    value={this.state.passwordInputValue}
+                                    type="password"
+                                    autoComplete="new-password"
+                                    placeholder="Password"
+                                    className={ this.state.credentialError || this.state.emailRequiredError ? "error" : null }
+                                />
+                                {
+                                    this.state.emailRequiredError ?
+                                        <div className="login-form-msg">
+                                            <span>Password field is required.</span>
+                                        </div> : null
+                                }
+                            </div>
+
+                            <div className="login-form-submit-btn-container">
+                                {
+                                    !this.state.loading ?
+                                        <div onClick={() => this.submitLoginRequest()} className="login-form-submit-btn">
+                                            <span>Login</span>
+                                        </div> :
+                                        <div className="login-form-submit-btn loading">
+                                            <span>Loading</span>
+                                        </div>
+                                }
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
