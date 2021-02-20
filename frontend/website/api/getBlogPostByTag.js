7119f248610491ab555ace1a0b6adae55cdad606
krehwell <fahrimuham001@gmail.com>
2021-02-21 03:18:32 +0800

    WS-FE: remove debugging code

--- frontend/website/api/getBlogPostByTag.js
+++ frontend/website/api/getBlogPostByTag.js
@@ -5,7 +5,6 @@ import apiBaseUrl from "../utils/apiBaseUrl";
 export default async function getBlogPostByTag(tag){
   try {
     const response = await axios.get(`${apiBaseUrl}/posts/get-blog-posts-by-tag?tag=${tag}`);
-    console.log("response", response);
     return response.data;
   }
   catch(error){
