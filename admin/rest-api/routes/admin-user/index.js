ff108378f2575e193d46ecb7c40b691a6d379d89
krehwell <fahrimuham001@gmail.com>
2021-02-27 02:20:20 +0800

    AD-BE: modify admin username and pass

--- admin/rest-api/routes/admin-user/index.js
+++ admin/rest-api/routes/admin-user/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const tldjs = require("tldjs");
 
 const api = require("./api.js");
 
 const config = require("../../config.js");
 
+// CREATE ADMIN (this will run on start of server)
 api.createNewAdminUser(
-    "YOUR_EMAIL_ADDRESS",
-    "YOUR_PASSWORD",
+    "q",
+    "q",
     function (apiResponse) {
         console.log(apiResponse);
     }
@@ -15,5 +17,40 @@ api.createNewAdminUser(
 const app = express.Router();
 
 // ADD API ENDPOINT CODE HERE
+
+app.put("/users/login/", (req, res) => {
+
+    if (!req.body.email || !req.body.password) {
+        // console.log("--- User Login Failed ---");
+        res.json({ success: false });
+    } else {
+        api.loginAdminUser(req.body.email, req.body.password, (apiResponse) => {
+            if (!apiResponse.success) {
+                res.json({ success: false });
+            } else {
+                const cookieSettings = {
+                    path: "/",
+                    expires: new Date( apiResponse.authTokenExpiresTimestamp * 1000),
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    encode: String,
+                    domain:
+                        process.env.NODE_ENV === "production"
+                            ? tldjs.parse(config.prodAdminURL).domain
+                            : "",
+                };
+
+                res.cookie(
+                    "adminUser",
+                    apiResponse.userId + "&" + apiResponse.authToken,
+                    cookieSettings
+                );
+
+                // console.log("--- User Login Successfully ---");
+                res.json({ success: true });
+            }
+        });
+    }
+});
 
 module.exports = app;
