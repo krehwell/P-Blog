230d4ed509d2a1f7eaa12efb7909ee054cddf7f4
krehwell <fahrimuham001@gmail.com>
2021-03-08 01:34:04 +0800

    WS-AD-FE-BE: setup config for production/deployment api url

--- admin/rest-api/config.js
+++ admin/rest-api/config.js
@@ -1,8 +1,8 @@
 const config = {
     devAdminURL: "http://localhost:3001",
-    prodAdminURL: "https://www.admin.example.com",
+    prodAdminURL: "https://admin.vercel.app",
     devFrontendWebsiteURL: "http://localhost:3000",
-    prodFrontendWebsiteURL: "https://www.example.com",
+    prodFrontendWebsiteURL: "https://krehwell.vercel.app",
 };
 
 module.exports = config;
