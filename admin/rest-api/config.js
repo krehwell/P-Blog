fb9e3a064cef2e14c1c95c08a4771240a97726d8
krehwell <fahrimuham001@gmail.com>
2021-03-10 09:42:50 +0800

    AD-BE-FE: trying to be able to allow cors on admin site #4

--- admin/rest-api/config.js
+++ admin/rest-api/config.js
@@ -1,6 +1,6 @@
 const config = {
     devAdminURL: "http://localhost:3001",
-    prodAdminURL: "https://admin-krehwell.vercel.app",
+    prodAdminURL: "https://admin.krehwell.com",
     devFrontendWebsiteURL: "http://localhost:3000",
     prodFrontendWebsiteURL: "https://krehwell.vercel.app",
 };
--- admin/website/next.config.js
+++ admin/website/next.config.js
@@ -3,4 +3,17 @@ module.exports = {
         "DEV_ADMIN_API_URL": "http://localhost:5001",
         "PRODUCTION_ADMIN_API_URL": "https://admin-krehwell-rest-api.vercel.app",
     },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 }
