c2386e500fd6e0636c11f51f80966fb8a052a185
krehwell <fahrimuham001@gmail.com>
2021-03-12 01:15:45 +0800

    AD-FE-BE: fix config miss compared with development

--- admin/rest-api/config.js
+++ admin/rest-api/config.js
@@ -1,8 +1,6 @@
 const config = {
     devAdminURL: "http://localhost:3001",
     prodAdminURL: "https://admin-krehwell.vercel.app",
-    devFrontendWebsiteURL: "http://localhost:3000",
-    prodFrontendWebsiteURL: "https://krehwell.vercel.app",
 };
 
 module.exports = config;
