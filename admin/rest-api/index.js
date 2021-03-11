b7efff2ff2944bbbf1daa686865bb917e5d0b195
krehwell <fahrimuham001@gmail.com>
2021-03-11 20:57:35 +0800

    AD-BE: trying to be able to allow cors on admin site #6

--- admin/rest-api/index.js
+++ admin/rest-api/index.js
@@ -46,25 +46,9 @@ mongoose.connection.on("open", function () {
 app.use(helmet());
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Credentials', true)
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-	)
-	if (req.method === 'OPTIONS') {
-		res.status(200).end()
-		next();
-	}
-
+    res.sendStatus(200);
 	next();
 });
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
 
 app.use(
     cors({
