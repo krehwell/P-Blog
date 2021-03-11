4c1b94ffb15caf471e442dbe1c6fd1c25d685615
krehwell <fahrimuham001@gmail.com>
2021-03-11 22:58:39 +0800

    AD-FE: trying to be able to allow "cookies" on admin site #2

--- admin/website/next.config.js
+++ admin/website/next.config.js
@@ -10,7 +10,7 @@ module.exports = {
 				source: "/:path*",
 				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{ key: "Access-Control-Allow-Origin", value: "https://admin-krehbad-rest-api.herokuapp.com" },
 					{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
 					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
 				]
