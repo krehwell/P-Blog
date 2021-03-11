ff9da7a9994c39a75ba4f3b8a21121a37e7e67ec
krehwell <fahrimuham001@gmail.com>
2021-03-12 00:17:59 +0800

    AD-FE: trying to be able to allow "cookies" on admin site #3

--- admin/website/next.config.js
+++ admin/website/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         "DEV_ADMIN_API_URL": "http://localhost:5001",
-        "PRODUCTION_ADMIN_API_URL": "https://admin-krehbad-rest-api.herokuapp.com",
+		"PRODUCTION_ADMIN_API_URL": "http://admin-rest-api.krehwell.com/",
     },
 	async headers() {
 		return [
