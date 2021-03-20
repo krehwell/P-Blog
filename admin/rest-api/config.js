0b91761afc33ab5143873fe8a7118a6c7d12bf26
krehwell <fahrimuham001@gmail.com>
2021-03-20 20:50:50 +0800

    AD-BE: fix admin url on production

--- admin/rest-api/config.js
+++ admin/rest-api/config.js
@@ -1,6 +1,6 @@
 const config = {
     devAdminURL: "http://localhost:3001",
-    prodAdminURL: "https://admin-krehwell.vercel.app",
+    prodAdminURL: "https://admin.krehwell.com",
     prodFrontendWebsiteURL: "https://krehwell.com",
 };
 
