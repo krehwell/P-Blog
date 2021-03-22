4d77bd6a932b0caa814e41da8c8cf4b679ff23b0
krehwell <fahrimuham001@gmail.com>
2021-03-22 23:44:14 +0800

    WS-FE: preact migrated

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -10,4 +10,17 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
     dest: 'public'
   },
+
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    // https://github.com/leerob/leerob.io/blob/main/next.config.js
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom': 'preact/compat'
+      });
+    }
+
+    return config;
+  },
 });
