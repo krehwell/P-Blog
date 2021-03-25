4b0725a79f642379768339f194bb819ac232b37d
krehwell <fahrimuham001@gmail.com>
2021-03-25 16:26:57 +0800

    WS-FE: master migrating to preact

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -1,6 +1,10 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
+  images: {
+    domains: ['i.imgur.com'],
+  },
+
   env: {
     "DEV_API_URL": "http://localhost:5000",
     "PRODUCTION_API_URL": "https://krehwell-rest-api.vercel.app"
@@ -11,19 +15,15 @@ module.exports = withPWA({
     dest: 'public'
   },
 
-  /*
   webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
     // https://github.com/leerob/leerob.io/blob/main/next.config.js
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom': 'preact/compat'
-      });
-    }
+    Object.assign(config.resolve.alias, {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat'
+    });
 
     return config;
   },
-  */
 
 });
