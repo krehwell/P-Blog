c0ef89893b201ff1a3690ea0d9b8779f94f10620
krehwell <fahrimuham001@gmail.com>
2021-03-26 18:36:23 +0800

    WS-FE: redeploy failed url #2

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -7,7 +7,7 @@ module.exports = withPWA({
 
   env: {
     "DEV_API_URL": "http://localhost:5000",
-    "PRODUCTION_API_URL": "https://krehwell-rest-api.vercel.app/"
+    "PRODUCTION_API_URL": "https://krehwell-rest-api.vercel.app"
   },
 
   pwa: {
