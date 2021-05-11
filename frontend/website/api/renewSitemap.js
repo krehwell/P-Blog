1fbc0364644ec90f8c5c8a4c7b470266b5204f39
krehwell <fahrimuham001@gmail.com>
2021-05-11 16:42:37 +0800

    WS-FE-AD: serverless testing for sitemap update

--- frontend/website/api/renewSitemap.js
+++ frontend/website/api/renewSitemap.js
@@ -1,4 +1,33 @@
-export default function RenewSitemap(req, res) {
-    res.status = 200;
-    res.json({message: "it works"});
-}
+/**
+ * ENABLING CORS
+ * https://vercel.com/support/articles/how-to-enable-cors?query=cors#enabling-cors-in-a-single-node.js-serverless-function
+ */
+const allowCors = (fn) => async (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "https://admin.krehwell.com/");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "POST") {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+};
+
+const handler = (req, res) => {
+    if (!req.body.xml) {
+        res.json({updateSitemapError: true});
+    } else {
+        res.json({updateSitemapError: false});
+    }
+};
+
+module.exports = allowCors(handler);
