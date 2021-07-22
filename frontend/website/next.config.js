13da853fd87fd68916b37e1a15b6f09b7fa7393b
krehwell <fahrimuham001@gmail.com>
2021-07-23 03:08:35 +0800

    FE-BE: cleaning

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -1,28 +1,28 @@
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  images: {
-    domains: ['i.imgur.com'],
-  },
+    images: {
+        domains: ["i.imgur.com"],
+    },
 
-  env: {
-    "DEV_API_URL": "http://localhost:5000",
-    "PRODUCTION_API_URL": "https://krehwell-rest-api.vercel.app"
-  },
+    env: {
+        DEV_API_URL: "http://localhost:5000",
+        PRODUCTION_API_URL: "https://krehwell-rest-api.vercel.app",
+    },
 
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public'
-  },
+    pwa: {
+        disable: process.env.NODE_ENV === "development",
+        dest: "public",
+    },
 
-  webpack: (config) => {
-    // https://github.com/leerob/leerob.io/blob/main/next.config.js
-    Object.assign(config.resolve.alias, {
-      react: 'preact/compat',
-      'react-dom/test-utils': 'preact/test-utils',
-      'react-dom': 'preact/compat'
-    });
+    webpack: (config) => {
+        // https://github.com/leerob/leerob.io/blob/main/next.config.js
+        // Object.assign(config.resolve.alias, {
+        //   react: 'preact/compat',
+        //   'react-dom/test-utils': 'preact/test-utils',
+        //   'react-dom': 'preact/compat'
+        // });
 
-    return config;
-  },
+        return config;
+    },
 });
