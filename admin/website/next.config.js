16bd8bad2a0a20bf97fe29058f48fabbad22d044
krehwell <fahrimuham001@gmail.com>
2021-07-23 03:22:48 +0800

    fix super dumb error all this time

--- admin/website/next.config.js
+++ admin/website/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "DEV_ADMIN_API_URL": "http://localhost:5001",
-        "PRODUCTION_ADMIN_API_URL": "https://admin-rest-api.krehwell.com",
-    }
-}
+        DEV_ADMIN_API_URL: "http://localhost:5001",
+        PRODUCTION_ADMIN_API_URL: "https://admin-rest-api.krehwell.com",
+    },
+};
