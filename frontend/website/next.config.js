d85a6e858d846950b7b056cefdcb7c49f09dab6d
krehwell <fahrimuham001@gmail.com>
2021-07-23 03:28:24 +0800

    cleaning

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -1,19 +1,18 @@
-const withPWA = require('next-pwa');
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
     images: {
-        domains: ['i.imgur.com'],
+        domains: ["i.imgur.com"],
     },
 
     env: {
-        "DEV_API_URL": "http://localhost:5000",
-        "PRODUCTION_API_URL": "https://krehwell-rest-api.vercel.app"
+        DEV_API_URL: "http://localhost:5000",
+        PRODUCTION_API_URL: "https://krehwell-rest-api.vercel.app",
     },
 
     pwa: {
-        disable: process.env.NODE_ENV === 'development',
-        dest: 'public'
-
+        disable: process.env.NODE_ENV === "development",
+        dest: "public",
     },
 
     webpack: (config) => {
