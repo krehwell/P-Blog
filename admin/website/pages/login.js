048372d55af2d144e5bbf1be285a3bbdeb8f124c
krehwell <fahrimuham001@gmail.com>
2021-07-24 06:18:48 +0800

    AD-WS: login page to functional

--- admin/website/pages/login.js
+++ admin/website/pages/login.js
@@ -1,175 +1,152 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
+import Router from "next/router";
 
 import login from "../api/admin-user/login.js";
 import authUser from "../api/admin-user/auth.js";
 import removeAdminUserCookie from "../api/admin-user/removeAdminUserCookie.js";
 
-export default class extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: false,
-            credentialError: false,
-            emailInputValue: "",
-            emailRequiredError: false,
-            passwordInputValue: "",
-            passwordRequiredError: false,
-        };
-    }
+export default function Login() {
+    const [loading, setLoading] = useState(false);
+    const [credentialError, setCredentialError] = useState(false);
+    const [emailInputValue, setEmailInputValue] = useState("");
+    const [passwordInputValue, setPasswordInputValue] = useState("");
+    const [emailRequiredError, setEmailRequiredError] = useState(false);
+    const [passwordRequiredError, setPasswordRequiredError] = useState(false);
 
-    static async getInitialProps({ req, res }) {
-        const authResult = await authUser(req);
-
-        if (authResult.success) {
-            res.writeHead(302, { Location: "/" });
-            res.end();
-        }
-
-        return {};
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         removeAdminUserCookie();
-    }
+    }, []);
 
-    updateEmailInputValue = (event) => {
-        this.setState({
-            emailInputValue: event.target.value,
-            emailRequiredError: false,
-        });
+    const updateEmailInputValue = (e) => {
+        setEmailInputValue(e.target.value);
+        setEmailRequiredError(false);
     };
 
-    updatePasswordInputValue = (event) => {
-        this.setState({
-            passwordInputValue: event.target.value,
-            passwordRequiredError: false,
-        });
+    const updatePasswordInputValue = (e) => {
+        setPasswordInputValue(e.target.value);
+        setPasswordRequiredError(false);
     };
 
-    submitLoginRequest = () => {
-        if (!this.state.emailInputValue || !this.state.passwordInputValue) {
-            if (!this.state.emailInputValue) {
-                this.setState({ emailRequiredError: true });
+    const submitLoginRequest = () => {
+        if (!emailInputValue || !passwordInputValue) {
+            if (!emailInputValue) {
+                setEmailRequiredError(true);
             }
 
-            if (!this.state.passwordInputValue) {
-                this.setState({ passwordRequiredError: true });
+            if (!passwordInputValue) {
+                setPasswordRequiredError(true);
             }
         } else {
-            this.setState({ loading: true });
+            setLoading(true);
 
-            const self = this;
-
-            login(
-                this.state.emailInputValue,
-                this.state.passwordInputValue,
-                (apiResponse) => {
-                    if (!apiResponse.success) {
-                        self.setState({
-                            loading: false,
-                            credentialError: true,
-                            emailRequiredError: false,
-                            passwordRequiredError: false,
-                        });
-                    } else {
-                        window.location.href = "/";
-                    }
+            login(emailInputValue, passwordInputValue, (apiResponse) => {
+                if (!apiResponse.success) {
+                    setLoading(false);
+                    setCredentialError(true);
+                    setEmailRequiredError(false);
+                    setPasswordRequiredError(false);
+                } else {
+                    Router.push("/");
                 }
-            );
+            });
         }
     };
 
-    listenForEnterKeyPress = (event) => {
-        if (event.keyCode === 13 && this.state.passwordInputValue) {
-            this.submitLoginRequest();
+    const listenForEnterKeyPress = (e) => {
+        if (e.keyCode === 13 && passwordInputValue) {
+            submitLoginRequest();
         }
     };
 
-    render() {
-        return (
-            <div className="layout-wrapper">
-                <Head>
-                    <title>Login | Admin</title>
-                </Head>
-                <div className="login-wrapper">
-                    <div className="login-content-container">
-                        <div className="login-form-container">
-                            {this.state.credentialError ? (
-                                <div className="login-form-error-block">
-                                    <span>
-                                        Email address and/or password is
-                                        Incorrect
-                                    </span>
+    return (
+        <div className="layout-wrapper">
+            <Head>
+                <title>Login | Admin</title>
+            </Head>
+            <div className="login-wrapper">
+                <div className="login-content-container">
+                    <div className="login-form-container">
+                        {credentialError ? (
+                            <div className="login-form-error-block">
+                                <span>
+                                    Email address and/or password is Incorrect
+                                </span>
+                            </div>
+                        ) : null}
+
+                        <div className="login-form-top-header">
+                            <span>Admin Page</span>
+                        </div>
+
+                        <div className="login-form-field">
+                            <input
+                                onChange={updateEmailInputValue}
+                                value={emailInputValue}
+                                type="email"
+                                autoComplete="new-password"
+                                placeholder="Email Adress"
+                                className={
+                                    credentialError || emailRequiredError
+                                        ? "error"
+                                        : null
+                                }
+                            />
+                            {emailRequiredError ? (
+                                <div className="login-form-msg">
+                                    <span>Email field is required.</span>
                                 </div>
                             ) : null}
+                        </div>
 
-                            <div className="login-form-top-header">
-                                <span>Admin Page</span>
-                            </div>
+                        <div className="login-form-field">
+                            <input
+                                onChange={updatePasswordInputValue}
+                                onKeyDown={listenForEnterKeyPress}
+                                value={passwordInputValue}
+                                type="password"
+                                autoComplete="new-password"
+                                placeholder="Password"
+                                className={
+                                    credentialError || emailRequiredError
+                                        ? "error"
+                                        : null
+                                }
+                            />
+                            {emailRequiredError ? (
+                                <div className="login-form-msg">
+                                    <span>Password field is required.</span>
+                                </div>
+                            ) : null}
+                        </div>
 
-                            <div className="login-form-field">
-                                <input
-                                    onChange={this.updateEmailInputValue}
-                                    value={this.state.emailInputValue}
-                                    type="email"
-                                    autoComplete="new-password"
-                                    placeholder="Email Adress"
-                                    className={
-                                        this.state.credentialError ||
-                                        this.state.emailRequiredError
-                                            ? "error"
-                                            : null
-                                    }
-                                />
-                                {this.state.emailRequiredError ? (
-                                    <div className="login-form-msg">
-                                        <span>Email field is required.</span>
-                                    </div>
-                                ) : null}
-                            </div>
-
-                            <div className="login-form-field">
-                                <input
-                                    onChange={this.updatePasswordInputValue}
-                                    onKeyDown={this.listenForEnterKeyPress}
-                                    value={this.state.passwordInputValue}
-                                    type="password"
-                                    autoComplete="new-password"
-                                    placeholder="Password"
-                                    className={
-                                        this.state.credentialError ||
-                                        this.state.emailRequiredError
-                                            ? "error"
-                                            : null
-                                    }
-                                />
-                                {this.state.emailRequiredError ? (
-                                    <div className="login-form-msg">
-                                        <span>Password field is required.</span>
-                                    </div>
-                                ) : null}
-                            </div>
-
-                            <div className="login-form-submit-btn-container">
-                                {!this.state.loading ? (
-                                    <div
-                                        onClick={() =>
-                                            this.submitLoginRequest()
-                                        }
-                                        className="login-form-submit-btn">
-                                        <span>Login</span>
-                                    </div>
-                                ) : (
-                                    <div className="login-form-submit-btn loading">
-                                        <span>Loading</span>
-                                    </div>
-                                )}
-                            </div>
+                        <div className="login-form-submit-btn-container">
+                            {!loading ? (
+                                <div
+                                    onClick={() => submitLoginRequest()}
+                                    className="login-form-submit-btn">
+                                    <span>Login</span>
+                                </div>
+                            ) : (
+                                <div className="login-form-submit-btn loading">
+                                    <span>Loading</span>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
-        );
+        </div>
+    );
+}
+
+export async function getServerSideProps({ req, res }) {
+    const authResult = await authUser(req);
+
+    if (authResult.success) {
+        res.writeHead(302, { Location: "/" });
+        res.end();
     }
+
+    return { props: {} };
 }
