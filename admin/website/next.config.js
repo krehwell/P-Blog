d60b01edd3485072d8f69b0c5fbaca3f756f204b
krehwell <fahrimuham001@gmail.com>
2021-07-26 01:53:25 +0800

    AD-FE: enable pwa

--- admin/website/next.config.js
+++ admin/website/next.config.js
@@ -3,4 +3,21 @@ module.exports = {
         DEV_ADMIN_API_URL: "http://localhost:5001",
         PRODUCTION_ADMIN_API_URL: "https://admin-rest-api.krehwell.com",
     },
+
+    pwa: {
+        disable: process.env.NODE_ENV === "development",
+        dest: "public",
+    },
+
+    webpack: (config) => {
+        /* https://github.com/leerob/leerob.io/blob/main/next.config.js */
+
+        // Object.assign(config.resolve.alias, {
+        //   react: 'preact/compat',
+        //   'react-dom/test-utils': 'preact/test-utils',
+        //   'react-dom': 'preact/compat'
+        // });
+
+        return config;
+    },
 };
