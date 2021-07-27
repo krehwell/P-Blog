5f9203407423b95bd685b6e27932783e3e69e519
krehwell <fahrimuham001@gmail.com>
2021-07-27 10:36:45 +0800

    AD-FE: performance boost #1.1

--- admin/website/pages/login.js
+++ admin/website/pages/login.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import dynamic from "next/dynamic";
 
 import login from "../api/admin-user/login.js";
 import authUser from "../api/admin-user/auth.js";
