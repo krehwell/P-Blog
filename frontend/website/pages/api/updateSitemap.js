26bc910e97bea96854889b14a1b0bbef43cad73c
krehwell <fahrimuham001@gmail.com>
2022-02-12 15:47:25 +0700

    WS-FE: trying fix dir on prod

--- frontend/website/pages/api/updateSitemap.js
+++ frontend/website/pages/api/updateSitemap.js
@@ -2,11 +2,13 @@ import fs from "fs";
 import NextCors from "nextjs-cors";
 
 async function handler(req, res) {
+    const onDevMode = process.env.NODE_ENV === "development";
+
     // Run the cors middleware
     await NextCors(req, res, {
         // Options
         methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-        origin: !process.env.NODE_ENV === "development" ? "https://admin-rest-api.krehwell.com" : "*",
+        origin: onDevMode ? "*" : "https://admin-rest-api.krehwell.com",
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
@@ -14,9 +16,10 @@ async function handler(req, res) {
     const { body } = req;
     const newSitemap = body.newSitemap;
     console.log(typeof newSitemap);
+    console.log("cwd", process.cwd(), "__dirname", __dirname);
 
     try {
-        fs.writeFileSync(process.cwd() + "/public/sitemap.xml", newSitemap);
+        fs.writeFileSync(process.cwd() + onDevMode ? "" : "/.next/server" + "/public/sitemap.xml", newSitemap);
         res.status(200).send("Sitemap updated successfully");
     } catch (err) {
         console.error(err);
