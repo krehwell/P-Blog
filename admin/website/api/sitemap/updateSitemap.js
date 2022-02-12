8674379542e91668c980b00d81e7c51ec805ffd7
krehwell <fahrimuham001@gmail.com>
2022-02-12 15:31:11 +0700

    WS-AD: update sitemap to trigger serverless too

--- admin/website/api/sitemap/updateSitemap.js
+++ admin/website/api/sitemap/updateSitemap.js
@@ -2,10 +2,24 @@ import axios from "axios";
 
 import apiBaseUrl from "../../utils/apiBaseUrl.js";
 
+let frontEndProdUrl = "";
+if (process.env.NODE_ENV === "development") {
+    frontEndProdUrl = "http://localhost:3000";
+} else {
+    frontEndProdUrl = "https://krehwell.com";
+}
+
 export default function updateSitemap(callback) {
     axios
         .put(`${apiBaseUrl}/sitemap/update-xml-file`, {}, { withCredentials: true })
-        .then(function (response) {
+        .then(async function (response) {
+            /// trigger frontend to update it's sitemap
+            const updateSitemapRes = await axios.put(`${frontEndProdUrl}/api/updateSitemap`, {
+                newSitemap: response.data.xml,
+            });
+
+            console.log(updateSitemapRes);
+
             callback(response.data);
         })
         .catch(function (error) {
