8fbbc3a95cf32312a8c976c881199288f6ffac6c
krehwell <fahrimuham001@gmail.com>
2022-02-12 16:03:20 +0700

    WS-FE: cors shit

--- frontend/website/pages/api/updateSitemap.js
+++ frontend/website/pages/api/updateSitemap.js
@@ -8,7 +8,7 @@ async function handler(req, res) {
     await NextCors(req, res, {
         // Options
         methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-        origin: onDevMode ? "*" : "https://admin-rest-api.krehwell.com",
+        origin: "*",
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
