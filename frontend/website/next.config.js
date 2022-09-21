17e294863ea762bb7443d6e2e147d03869a9a4d8
krehwell <fahrimuham001@gmail.com>
2022-09-21 21:24:04 +0700

    remove preact

--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -14,18 +14,4 @@ module.exports = withPWA({
         disable: process.env.NODE_ENV === "development",
         dest: "public",
     },
-
-    webpack: (config) => {
-        /* https://github.com/leerob/leerob.io/blob/main/next.config.js */
-
-        // Object.assign(config.resolve.alias, {
-        //   react: 'preact/compat',
-        //   'react-dom/test-utils': 'preact/test-utils',
-        //   'react-dom': 'preact/compat'
-        // });
-
-        return config;
-    },
-
-    swcMinify: true,
 });
